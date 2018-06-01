89e9fba00b8a40b5e47c0f69d6e7b450025de6d8
iroegbu <iroegbuogechi@gmail.com>
2018-06-01 22:28:22 +0100

    adding graph properties

---
+++ ClientVerifierLibrary/Graph/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientVerifierLibrary.Graph
+{
+    class Node<T>
+    {
+        public T Value { get; set; }
+        public int Cost { get; set; }
+        public int Heuristics { get; set; }
+        public Node<T> Parent { get; set; }
+        public List<Node<T>> Children { get; set; }
+    }
+}
---
+++ ClientVerifierLibrary/Graph/PriorityQueue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientVerifierLibrary.Graph
+{
+    class PriorityQueue<T> : Collection<Node<T>>
+    {
+        public Node<T> Pop()
+        {
+            var node = Items.OrderBy(n => n.Heuristics + n.Cost).First();
+            Remove(node);
+            return node;
+        }
+    }
+}
